4f4dfec19e9094385a29a14674e127535911c643
jeroendedauw <jeroendedauw@gmail.com>
2013-07-30 15:58:43 +0200

    Finish initial implementation of IndexDefinition
    Change-Id: I05290e0ce4e2b3459436632ddecc81ccab37db32

--- tests/phpunit/IndexDefinitionTest.php
+++ tests/phpunit/IndexDefinitionTest.php
@@ -21,12 +21,23 @@ use Wikibase\Database\IndexDefinition;
  */
 class IndexDefinitionTest extends \PHPUnit_Framework_TestCase {
 
-	public function testCanGetName() {
-		$indexName = 'foo_bar';
-
+	/**
+	 * @dataProvider validNameProvider
+	 */
+	public function testCanGetName( $indexName ) {
 		$index = new IndexDefinition( $indexName, array( 'a' => 0 ) );
 
 		$this->assertEquals( $indexName, $index->getName() );
+	}
+
+	public function validNameProvider() {
+		return array(
+			array( 'f' ),
+			array( 'f' ),
+			array( 'foo_bar' ),
+			array( 'foo_bar_baz' ),
+			array( 'foobarbazbah' ),
+		);
 	}
 
 	/**
@@ -90,6 +101,41 @@ class IndexDefinitionTest extends \PHPUnit_Framework_TestCase {
 			array( array( 'name' => 4.2 ) ),
 			array( array( 'name' => -1 ) ),
 			array( array( 'name' => -1337 ) ),
+		);
+	}
+
+	public function testDefaultIndexTypeIsSet() {
+		$index = new IndexDefinition( 'foo_bar', array( 'a' => 0 ) );
+
+		$this->assertEquals( IndexDefinition::TYPE_INDEX, $index->getType() );
+	}
+
+	public function testGetSetIndexType() {
+		$indexType = IndexDefinition::TYPE_FULLTEXT;
+
+		$index = new IndexDefinition( 'foo_bar', array( 'a' => 0 ), $indexType );
+
+		$this->assertEquals( $indexType, $index->getType() );
+	}
+
+	/**
+	 * @dataProvider invalidIndexTypeProvider
+	 */
+	public function testCannotSetInvalidIndexType( $notAnIndexType ) {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		new IndexDefinition( 'foo_bar', array( 'a' => 0 ), $notAnIndexType );
+	}
+
+	public function invalidIndexTypeProvider() {
+		return array(
+			array( null ),
+			array( array() ),
+			array( false ),
+			array( 42 ),
+			array( 4.2 ),
+			array( (object)array( 'foo' => 'bar' ) ),
+			array( '' ),
+			array( 'foo' ),
 		);
 	}
 
