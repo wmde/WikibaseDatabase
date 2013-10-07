27b4a1fc77fa8ebbe82fdb95de9149c52ab19559
jeroendedauw <jeroendedauw@gmail.com>
2013-10-07 16:50:33 +0200

    Improve MySQLSchemaSqlBuilderTest
    Change-Id: I582e91f48316bc54ac1e03a53ca8bb181d94030e

--- tests/phpunit/MySQL/MySQLSchemaSqlBuilderTest.php
+++ tests/phpunit/MySQL/MySQLSchemaSqlBuilderTest.php
@@ -21,12 +21,16 @@ class MySQLSchemaSqlBuilderTest extends \PHPUnit_Framework_TestCase {
 		$mockEscaper = $this->getMock( 'Wikibase\Database\Escaper' );
 		$mockEscaper->expects( $this->any() )
 			->method( 'getEscapedValue' )
-			->will( $this->returnArgument(0) );
+			->will( $this->returnCallback( function( $input ) {
+				return "|$input|";
+			} ) );
 
 		$mockTableNameFormatter = $this->getMock( 'Wikibase\Database\TableNameFormatter' );
 		$mockTableNameFormatter->expects( $this->atLeastOnce() )
 			->method( 'formatTableName' )
-			->will( $this->returnArgument(0) );
+			->will($this->returnCallback( function( $input ) {
+				return "||$input||";
+			} ) );
 
 		return new MySQLSchemaSqlBuilder( $mockEscaper, $mockTableNameFormatter );
 	}
@@ -34,20 +38,20 @@ class MySQLSchemaSqlBuilderTest extends \PHPUnit_Framework_TestCase {
 	public function testGetRemoveFieldSql(){
 		$instance = $this->newInstance();
 		$sql = $instance->getRemoveFieldSql( 'tableName', 'fieldName' );
-		$this->assertEquals( "ALTER TABLE tableName DROP fieldName", $sql );
+		$this->assertEquals( "ALTER TABLE ||tableName|| DROP fieldName", $sql );
 	}
 
 	public function testGetAddFieldSql(){
 		$instance = $this->newInstance();
 		$field = new FieldDefinition( 'intField', FieldDefinition::TYPE_INTEGER, FieldDefinition::NOT_NULL, 42 );
 		$sql = $instance->getAddFieldSql( 'tableName', $field );
-		$this->assertEquals( 'ALTER TABLE tableName ADD intField INT DEFAULT 42 NOT NULL', $sql );
+		$this->assertEquals( 'ALTER TABLE ||tableName|| ADD intField INT DEFAULT |42| NOT NULL', $sql );
 	}
 
 	public function testGetRemoveIndexSql(){
 		$instance = $this->newInstance();
 		$sql = $instance->getRemoveIndexSql( 'tableName', 'indexName' );
-		$this->assertEquals( "DROP INDEX indexName ON tableName", $sql );
+		$this->assertEquals( "DROP INDEX indexName ON ||tableName||", $sql );
 	}
 
 }
