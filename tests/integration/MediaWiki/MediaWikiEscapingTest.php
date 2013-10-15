b5541321c10aca03b2779b836e323a324373d154
jeroendedauw <jeroendedauw@gmail.com>
2013-10-15 20:58:58 +0200

    Add MediaWikiEscapingTest
    Change-Id: I60c7645ab85bb7d623577b6cec226f46e09f0b95

---
+++ tests/integration/MediaWiki/MediaWikiEscapingTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Wikibase\Database\Tests;
+
+use Wikibase\Database\MediaWiki\MediaWikiEscaper;
+use Wikibase\Database\Schema\Definitions\FieldDefinition;
+use Wikibase\Database\Schema\Definitions\IndexDefinition;
+use Wikibase\Database\Schema\Definitions\TableDefinition;
+
+/**
+ * @since 0.1
+ *
+ * @group Wikibase
+ * @group WikibaseDatabase
+ * @group Integration
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class MediaWikiEscapingTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @dataProvider valueEscapingProvider
+	 */
+	public function testValueEscaping( $input, $expectedOutput ) {
+		$escaper = new MediaWikiEscaper( wfGetDB( DB_SLAVE ) );
+		$output = $escaper->getEscapedValue( $input );
+
+		$this->assertEquals(
+			$expectedOutput,
+			$output
+		);
+	}
+
+	public function valueEscapingProvider() {
+		return array(
+			array( 'foo', "'foo'" ),
+			array( '', "''" ),
+			array( '42', "'42'" ),
+			array( 42, "'42'" ),
+		);
+	}
+
+}
