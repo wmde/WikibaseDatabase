f007fdea3fa64bb479ebc14fa77580dd15caf9a2
jeroendedauw <jeroendedauw@gmail.com>
2013-12-01 05:42:28 +0100

    Run composer update from PHPUnit bootstrap
    Change-Id: Ieb6411b90c531ca6fff891e2be66387209ea1135

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+echo exec( 'composer update' ) . "\n";
+
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
