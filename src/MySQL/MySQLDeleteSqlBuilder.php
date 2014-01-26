324622444412f835b83b0e15381e4fb2969d2341
jeroendedauw <jeroendedauw@gmail.com>
2014-01-26 23:50:30 +0100

    Implementation work on MySQLDeleteSqlBuilder

--- src/MySQL/MySQLDeleteSqlBuilder.php
+++ src/MySQL/MySQLDeleteSqlBuilder.php
@@ -3,6 +3,7 @@
 namespace Wikibase\Database\MySQL;
 
 use Wikibase\Database\QueryInterface\DeleteSqlBuilder;
+use Wikibase\Database\ValueEscaper;
 
 /**
  * @since 0.2
@@ -10,6 +11,12 @@ use Wikibase\Database\QueryInterface\DeleteSqlBuilder;
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class MySQLDeleteSqlBuilder implements DeleteSqlBuilder {
+
+	protected $escaper;
+
+	public function __construct( ValueEscaper $escaper ) {
+		$this->escaper = $escaper;
+	}
 
 	/**
 	 * @see DeleteSqlBuilder::getDeleteSql
@@ -20,7 +27,31 @@ class MySQLDeleteSqlBuilder implements DeleteSqlBuilder {
 	 * @return string
 	 */
 	public function getDeleteSql( $tableName, array $conditions ) {
-		// TODO: Implement getDeleteSql() method.
+		$sql = 'DELETE FROM ' . $tableName;
+
+		if ( !empty( $conditions ) ) {
+			$sql .= ' ' . $this->getConditionSql( $conditions );
+		}
+
+		return $sql;
+	}
+
+	protected function getConditionSql( array $conditions ) {
+		$expandedConditions = array();
+
+		foreach ( $conditions as $key => $value ) {
+			$expandedConditions[] = $this->expandCondition( $key, $value );
+		}
+
+		return 'WHERE ' . implode( ' AND ', $expandedConditions );
+	}
+
+	protected function expandCondition( $key, $value ) {
+		if ( is_numeric( $key ) ) {
+			return $value;
+		}
+
+		return $key . '=' . $this->escaper->getEscapedValue( $value );
 	}
 
 }
--- tests/phpunit/MySQL/MySQLDeleteSqlBuilderTest.php
+++ tests/phpunit/MySQL/MySQLDeleteSqlBuilderTest.php
@@ -17,8 +17,81 @@ use Wikibase\Database\MySQL\MySQLDeleteSqlBuilder;
  */
 class MySQLDeleteSqlBuilderTest extends \PHPUnit_Framework_TestCase {
 
-	public function testTodo() {
-		$this->assertTrue( (bool)'TODO' );
+	/**
+	 * @var MySQLDeleteSqlBuilder
+	 */
+	protected $sqlBuilder;
+
+	public function setUp() {
+		$escaper = $this->getMock( 'Wikibase\Database\ValueEscaper' );
+
+		$escaper->expects( $this->any() )
+			->method( 'getEscapedValue' )
+			->will( $this->returnCallback( function( $value ) {
+				return '|' . $value . '|';
+			} ) );
+
+		$this->sqlBuilder = new MySQLDeleteSqlBuilder( $escaper );
+	}
+
+	public function testGivenNoConditions_noConditionsAreInSql() {
+		$this->assertTableAndConditionsResultInSql(
+			'some_table',
+			array(),
+			'DELETE FROM some_table'
+		);
+	}
+
+	protected function assertTableAndConditionsResultInSql( $tableName, array $conditions, $sql ) {
+		$this->assertSame(
+			$sql,
+			$this->sqlBuilder->getDeleteSql( $tableName, $conditions )
+		);
+	}
+
+	public function testGivenOneKeyValuePair_returnsOneEqualityCondition() {
+		$this->assertTableAndConditionsResultInSql(
+			'some_table',
+			array(
+				'some_field' => 'foobar'
+			),
+			'DELETE FROM some_table WHERE some_field=|foobar|'
+		);
+	}
+
+	public function testGivenTwoKeyValuePairs_returnsTwoEqualityConditions() {
+		$this->assertTableAndConditionsResultInSql(
+			'some_table',
+			array(
+				'some_field' => 'foobar',
+				'another_field' => 42
+			),
+			'DELETE FROM some_table WHERE some_field=|foobar| AND another_field=|42|'
+		);
+	}
+
+	public function testGivenManyKeyValuePairs_returnsManyEqualityConditions() {
+		$this->assertTableAndConditionsResultInSql(
+			'some_table',
+			array(
+				'some_field' => 'foobar',
+				'another_field' => 42,
+				'third_field' => ''
+			),
+			'DELETE FROM some_table WHERE some_field=|foobar| AND another_field=|42| AND third_field=||'
+		);
+	}
+
+	public function testGivenNoKeys_returnsConditionsAsProvided() {
+		$this->assertTableAndConditionsResultInSql(
+			'some_table',
+			array(
+				'some_field=foobar',
+				'another_field' => 42,
+				'third_field > 9000'
+			),
+			'DELETE FROM some_table WHERE some_field=foobar AND another_field=|42| AND third_field > 9000'
+		);
 	}
 
 }
