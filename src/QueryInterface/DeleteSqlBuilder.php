f7129972bafc593ecbf9e0f7683eabcb583cbf86
jeroendedauw <jeroendedauw@gmail.com>
2014-01-27 00:57:35 +0100

    Update DeleteSqlBuilder docs

--- src/QueryInterface/DeleteSqlBuilder.php
+++ src/QueryInterface/DeleteSqlBuilder.php
@@ -13,6 +13,13 @@ interface DeleteSqlBuilder {
 	 * @param string $tableName
 	 * @param array $conditions
 	 *
+	 * The conditions can be provided as stings, in which case
+	 * they are used as-is. They can also be provided as key value
+	 * pairs, where the (array) field key is the field name. In this
+	 * case an equality condition is build, and the value is escaped.
+	 * This value can also be an array, in which case they values
+	 * are treated as disjunction and each escaped.
+	 *
 	 * @return string
 	 */
 	public function getDeleteSql( $tableName, array $conditions );
