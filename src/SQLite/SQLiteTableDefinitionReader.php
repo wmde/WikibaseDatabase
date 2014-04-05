5f177b5f44a5f9cefe5961ea2f81a60effd3f94e
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-04-05 10:48:01 +0200

    Use easier to read shortcuts in SQL parser regular expressions

--- src/SQLite/SQLiteTableDefinitionReader.php
+++ src/SQLite/SQLiteTableDefinitionReader.php
@@ -86,7 +86,7 @@ class SQLiteTableDefinitionReader implements TableDefinitionReader {
 		foreach( $results as $result ){
 			$sql = preg_replace( '/, PRIMARY KEY \([^\)]+\)/i', '', $result->sql );
 			/** $createParts,  1 => tableName, 2 => fieldParts (fields, keys, etc.) */
-			$matchedCreate = preg_match( '/CREATE TABLE ([^ ]+) \((.+)\)$/i', $sql, $createParts );
+			$matchedCreate = preg_match( '/CREATE TABLE (\S+) \((.+)\)$/i', $sql, $createParts );
 			if( $matchedCreate !== 1 ){
 				throw new SchemaReadingException(
 					"Failed to match CREATE TABLE regex with sql string: " . $sql
@@ -95,7 +95,7 @@ class SQLiteTableDefinitionReader implements TableDefinitionReader {
 
 			foreach( explode( ',', $createParts[2] ) as $fieldSql ) {
 				$matchedParts = preg_match(
-					'/([^\s]+) ([^\s]+)( DEFAULT ([^\s]+))?( ((NOT )?NULL))?( (PRIMARY KEY AUTOINCREMENT))?/i',
+					'/(\S+) (\S+)( DEFAULT (\S+))?( ((NOT )?NULL))?( (PRIMARY KEY AUTOINCREMENT))?/i',
 					$fieldSql,
 					$fieldParts
 				);
@@ -276,7 +276,7 @@ class SQLiteTableDefinitionReader implements TableDefinitionReader {
 	 */
 	private function getIndex( $sql, $tableName ) {
 		$matchResult = preg_match(
-			'/CREATE (INDEX|UNIQUE INDEX) ([^ ]+) ON ([^ ]+) \((.+)\)\z/i',
+			'/CREATE (INDEX|UNIQUE INDEX) (\S+) ON (\S+) \((.+)\)\z/i',
 			$sql,
 			$createParts
 		);
@@ -340,9 +340,9 @@ class SQLiteTableDefinitionReader implements TableDefinitionReader {
 	}
 
 	private function getPrimaryKey( $sql ) {
-		if( preg_match( '/PRIMARY KEY \(([^\)]+)\)/i', $sql, $createParts ) ) {
+		if( preg_match( '/PRIMARY KEY \(([^)]+)\)/i', $sql, $createParts ) ) {
 			return $this->getPrimaryKeyForFields( $createParts[1] );
-		} else if( preg_match( '/(\(|,| )+([^ ]+)[a-z0-9 _]+PRIMARY KEY AUTOINCREMENT/i', $sql, $fieldParts ) ) {
+		} else if( preg_match( '/[(, ]+(\S+)[\w ]+PRIMARY KEY AUTOINCREMENT/i', $sql, $fieldParts ) ) {
 			return $this->getPrimaryKeyForField( $fieldParts[2] );
 		}
 		throw new RuntimeException( __CLASS__ . " can not read primary ky from sql '{$sql}'" );
