1b7fabaa2630633d758e2b9f4cf5464a246e9c46
jeroendedauw <jeroendedauw@gmail.com>
2014-10-22 11:48:45 +0200

    Update PHPUnit boottsrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,14 +9,12 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
-if ( php_sapi_name() !== 'cli' ) {
+if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-$pwd = getcwd();
-chdir( __DIR__ . '/..' );
-passthru( 'composer update' );
-chdir( $pwd );
+error_reporting( E_ALL | E_STRICT );
+ini_set( 'display_errors', 1 );
 
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
