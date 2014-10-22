3928a48d8cfa5ec13c90b606236e8fafb76d7ae0
jeroendedauw <jeroendedauw@gmail.com>
2014-10-22 11:38:07 +0200

    Update git repo path of MW core

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -12,9 +12,9 @@ function runMediaWikiSuite {
 function installMediaWiki {
 	cd ..
 
-	wget https://github.com/wikimedia/mediawiki-core/archive/$MW.tar.gz
+	wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
     tar -zxf $MW.tar.gz
-    mv mediawiki-core-$MW phase3
+    mv mediawiki-$MW phase3
 
     cd phase3
 
